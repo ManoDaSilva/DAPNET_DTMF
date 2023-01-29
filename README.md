9f367f879826a3e3a184f138485a80ad17e9a3a3
Manoel <manoel.casquilho@gmail.com>
2023-01-29 23:55:04 +0100

    Added system and unipager status

--- README.md
+++ README.md
@@ -7,7 +7,14 @@ An additional Arduino takes care of translating this logic into Serial, and spit
 A small python script then takes this info and transmits canned messages to unipager via a Websocket connection
 
 # Features
-So far, I only implemented canned messages. In the future, one could request the IP address, system status, etc.
+You can send messages via DTMF by dialing:
+```
+#Ric*MessageNr#
+```
+You can set up to 90 canned messages. 91 to 99 have been blocked for "special" messages. So far, those have been implemented:
+* 98: Unipager status (server, callsign, connection status)
+* 99: System status (CPU temp, IP Address)
+
 
 # Getting started
 
@@ -20,21 +27,18 @@ Install unipager and set it up the classic way.
 
 Install pip and git:
 ```bash
-	sudo apt install git pip
+sudo apt install git pip
 ```
 Install required python modules:
 ```bash
-	sudo pip install websocket-client pyserial
+sudo pip install websocket-client pyserial
 ```
 Edit interface.py and change the serial interface if needed (by default: first USB serial adapter). If you're using directly the GPIO UART, you might need to disable the serial console or others, I haven't looked it up just yet.	
 Edit the canned messages as your heart desires.
 	
-Start the script. You can send messages via DTMF by dialing:
-```
-	#Ric*MessageNr#
-```
+Start the script, it should now be ready to receive DTMF calls.
 
 
 # Credits
-https://github.com/dk4pa/UniPager-SendWebsocket
-https://electropeak.com/learn/interfacing-mt8870-dtmf-decoder-module-with-arduino/
+* https://github.com/dk4pa/UniPager-SendWebsocket
+* https://electropeak.com/learn/interfacing-mt8870-dtmf-decoder-module-with-arduino/
--- interface.py
+++ interface.py
@@ -2,44 +2,93 @@ import json
 import pprint
 import websocket
 from websocket import create_connection
-
+import socket
 import serial
-import io
 import time
+import json
+from urllib.request import urlopen
 
-cannedMsg = ["Test de Couverture","R0 B-EARS. RDV sur Relais ou 145.500MHz","Canned msg 2", "Canned msg 3", "Canned msg 4", "Canned msg 5"]
 
-
+cannedMsg = [
+    "Test de Couverture",
+    "R0 B-EARS. RDV sur Relais ou 145.500MHz",
+    "Canned msg 2",
+    "Canned msg 3",
+    "Canned msg 4",
+    "Canned msg 5",
+]
 def sendToUnipager(ric, text, m_type, m_func):
+    time.sleep(2)
     websocket.enableTrace(True)
-    ws = create_connection('ws://localhost:8055/')
-    string_to_send = "{\"SendMessage\": {\"addr\": %s, \"data\": \"%s\", \"mtype\": \"%s\", \"func\": \"%s\"}}" % (ric, text, m_type, m_func)
+    ws = create_connection("ws://localhost:8055/")
+    string_to_send = (
+        '{"SendMessage": {"addr": %s, "data": "%s", "mtype": "%s", "func": "%s"}}'
+        % (ric, text, m_type, m_func)
+    )
     ws.send(string_to_send)
+
+
+def sendSysStatus():
+    hostname = socket.gethostname()
+    ## getting the IP address using socket.gethostbyname() method
+    ipAddress = socket.gethostbyname(hostname)
+    rawCpuTemp = 0
+    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        rawCpuTemp = f.read()
+    cpuTemp = int(rawCpuTemp) / 1000
+    sysInfo = "CPU Temp: {} C, IP Addr: {}".format(cpuTemp, ipAddress)
+    return sysInfo
     
-ser = serial.Serial('/dev/ttyACM0',
-                        baudrate=9600,
-                        bytesize=serial.EIGHTBITS,
-                        parity=serial.PARITY_NONE,
-                        stopbits=serial.STOPBITS_ONE,
-                        timeout=None)
+def sendUnipagerStatus():
+    # Opening JSON file
+    f = open('/var/lib/unipager/config.json')
+    data = json.load(f)
+    callsign = data['master']['call']
+    f.close()
+    
+    response = urlopen("http://127.0.0.1:8073/status")
+    data = json.loads(response.read())
+    if data['connected'] == True:
+        status = "Connected"
+    else:
+        status = "Disconnected"
+    server = data['master']
+    unipagerInfo = "Callsign: {}, Server: {}, Status: {}".format(callsign, server, status)
+    return unipagerInfo
+
+
+ser = serial.Serial(
+    "/dev/ttyACM0",
+    baudrate=9600,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=None,
+)
 
 while True:
     data = ""
-    dataRaw = ser.read_until(b'\x23')
+    dataRaw = ser.read_until(b"\x23")
     print(str(dataRaw))
-    dataUTF = dataRaw.decode('utf-8')
+    dataUTF = dataRaw.decode("utf-8")
     dataUTF = dataUTF[:-1]
-    dataProcessed = dataUTF.split("*",2)
+    dataProcessed = dataUTF.split("*", 2)
     print(dataProcessed)
     if len(dataProcessed) == 2:
-        time.sleep(2)
         try:
             msgIndex = int(dataProcessed[1])
-                if 0 <= msgIndex < len(cannedMsg):
-                sendToUnipager(dataProcessed[0],cannedMsg[msgIndex],"AlphaNum","Func3")
+            if 0 <= msgIndex < len(cannedMsg):
+                sendToUnipager(
+                    dataProcessed[0], cannedMsg[msgIndex], "AlphaNum", "Func3"
+                )
+            elif msgIndex == 99:
+                sendToUnipager(dataProcessed[0], sendSysStatus(), "AlphaNum", "Func3")
+            elif msgIndex == 98:
+                sendToUnipager(dataProcessed[0], sendUnipagerStatus(), "AlphaNum", "Func3")
+            else:
+                print("Invalid msg index or RIC range")
         except:
             print("Invalid or empty fields")
-        else:
-            print("Invalid msg index or RIC range")
+
     else:
-        print("Incorrect MSG lenght")
+        print("Incorrect message length")
